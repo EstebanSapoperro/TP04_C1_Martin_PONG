dafa60876565d98d6aa23cfc42863ee0c344f0d6
EstebanSapoperro <martinestebangarciaa@gmail.com>
2026-09-09 14:16:33 -0300

    Add void Mechanic

--- Assets/Scripts/Characters/Voidmechanic.cs
+++ Assets/Scripts/Characters/Voidmechanic.cs
@@ -3,19 +3,72 @@ using UnityEngine;
 public class Voidmechanic : MonoBehaviour
 {
 
+    [SerializeField] private float basecooldown = 7.0f;
+
+
     [SerializeField] private SpriteRenderer voided;
-    private float cooldowntimer = 7.0f;
+    [SerializeField] private SpriteRenderer playerOne;
+    [SerializeField] private SpriteRenderer playerTwo;
+    private float cooldowntimer;
+    private bool priWarning = false;
+    private bool secWarning = false;
+    private bool treWarning = false;
+    private bool danger = false;
+
+    private float countPriWarning = 3;
+    private float countSecWarning = 2;
+    private float countTreWarning = 1;
+    private float countDanger = 0.5f;
+
     void Start()
     {
-        
+        cooldowntimer = basecooldown;
     }
     private void FixedUpdate()
     {
-        if (cooldowntimer >= 0) 
+        if (cooldowntimer >= -1) 
         {
          cooldowntimer -= Time.deltaTime;
         }
-       
+
+        if ((cooldowntimer <= countPriWarning)&&(priWarning == false)) 
+        {
+            Debug.Log("llego al timer1");
+            voided.color = Color.darkBlue;
+            priWarning = true;
+        }
+
+        if ((cooldowntimer <= countSecWarning) && (secWarning == false))
+        {
+            Debug.Log("llego al timer3");
+            voided.color = Color.darkRed;
+            secWarning = true;
+        }
+
+        if ((cooldowntimer <= countTreWarning) && (treWarning == false))
+        {
+            Debug.Log("llego al Danegr");
+            voided.color = Color.red;
+            treWarning = true;
+        }
+
+        if ((cooldowntimer <= countDanger) && (danger == false))
+        {
+            Debug.Log("llego al timer2");
+            voided.color = Color.black;
+            danger = true;
+        }
+
+        if (cooldowntimer <= 0)
+        {
+            voided.color = Color.blue;
+            cooldowntimer = basecooldown;
+            danger = false;
+            priWarning = false;
+            secWarning = false;
+            treWarning = false;
+        }
+
     }
 
 }
